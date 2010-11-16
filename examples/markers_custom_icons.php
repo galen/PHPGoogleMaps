52dd71fbc781143950ae48b9da2fb6a23ca4593c
galen <galenjr@gmail.com>
2010-11-16 01:08:52 -0500

    add custom marker icon example

---
+++ examples/markers_custom_icons.php
@@ -0,0 +1,47 @@
+<?php
+
+require( '../google_maps.php' );
+require( '_system/config.php' );
+
+$map = new \googlemaps\GoogleMap();
+
+$marker_options = array(
+	'title'		=> 'Custom marker icon',
+	'content'	=> 'This marker uses a custom icon'
+);
+$marker1 = \googlemaps\overlay\Marker::createFromLocation( 'New York, NY', $marker_options );
+$icon = new \googlemaps\overlay\MarkerIcon( '_images/yellow_marker.png' );
+$shadow = new \googlemaps\overlay\MarkerIcon( '_images/yellow_marker_shadow.png' );
+$shadow->setAnchor( 11 );
+$marker1->setIcon( $icon )->setShadow( $shadow );
+
+$marker_options = array(
+	'title'		=> 'Custom marker icon',
+	'content'	=> 'This marker uses a custom icon'
+);
+$marker2 = \googlemaps\overlay\Marker::createFromLocation( 'New Haven, CT', $marker_options );
+$icon2 = \googlemaps\overlay\MarkerIcon::create( '_images/purple_marker_sprite.png', array( 'height' => 34 ) );
+$shadow2 = new \googlemaps\overlay\MarkerIcon( '_images/purple_marker_sprite.png', array( 'height' => 24, 'origin_x' => 0, 'origin_y' => 34, 'anchor_x' => 11, 'anchor_y' => 24 ) );
+$marker2->setIcon( $icon2 )->setShadow( $shadow2 );
+$map->addObjects( array( $marker1, $marker2 ) );
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Custom Marker Icons - <?php echo PAGE_TITLE ?></title>
+	<link rel="stylesheet" type="text/css" href="_css/style.css">
+	<?php $map->printHeaderJS() ?>
+	<?php $map->printMapJS() ?>
+</head>
+<body>
+
+<h1>Custom Marker Icons</h1>
+<?php require( '_system/nav.php' ) ?>
+
+<?php $map->printMap() ?>
+
+</body>
+
+</html>
