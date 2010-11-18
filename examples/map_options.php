cc7d8781e8c41ece0a8bb0ca54f4f967289398d0
galen <galenjr@gmail.com>
2010-11-18 08:19:39 -0500

    add map options example

---
+++ examples/map_options.php
@@ -0,0 +1,36 @@
+<?php
+
+require( '../PHPGoogleMaps/PHPGoogleMaps.php' );
+require( '_system/config.php' );
+
+$map_options = array(
+	'map_id'		=> 'map23',
+	'draggable'		=> false,
+	'center'		=> 'San Diego, CA',
+	'height'		=> '600px',
+	'width'			=> '600px',
+	'zoom'			=> 10,
+	'bicycle_layer'	=> true
+);
+$map = new \googlemaps\GoogleMap( $map_options );
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Map Options - <?php echo PAGE_TITLE ?></title>
+	<link rel="stylesheet" type="text/css" href="_css/style.css">
+	<?php $map->printHeaderJS() ?>
+	<?php $map->printMapJS() ?>
+</head>
+<body>
+
+<h1>Map Options</h1>
+<?php require( '_system/nav.php' ) ?>
+
+<?php $map->printMap() ?>
+
+</body>
+
+</html>
