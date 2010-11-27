093a977ffb73d52f1e86315d8044472d156988b2
galen <galenjr@gmail.com>
2010-11-27 16:45:03 -0500

    Update streetview example

--- examples/streetview.php
+++ examples/streetview.php
@@ -8,8 +8,12 @@ $map->enableStreetView();
 $map->setCenter('New Haven, CT');
 
 $map2 = new \GoogleMaps\Map( array( 'map_id' => 'map2' ) );
-$map2->enableStreetView( null, 'container' );
+$map2->enableStreetView( array( 'addressControl' => false, 'enableCloseButton' => false ), 'container' );
 $map2->setCenter('San Diego, CA');
+
+$map3 = new \GoogleMaps\Map( array( 'map_id' => 'map3' ) );
+$map3->enableStreetView( array( 'position' => \GoogleMaps\Service\Geocoder::getLatLng( 'San Diego, CA' ) ) );
+$map3->setCenter('Houston, TX');
 
 ?>
 
@@ -21,6 +25,7 @@ $map2->setCenter('San Diego, CA');
 	<?php $map->printHeaderJS() ?>
 	<?php $map->printMapJS() ?>
 	<?php $map2->printMapJS() ?>
+	<?php $map3->printMapJS() ?>
 	<style type="text/css">
 	#map2, #container { float:left }
 	</style>
@@ -30,12 +35,17 @@ $map2->setCenter('San Diego, CA');
 <h1>Streetview</h1>
 <?php require( '_system/nav.php' ) ?>
 
-<h2>Same container</h2>
+<h2>Same Container</h2>
 <?php $map->printMap() ?>
 <hr>
 <h2>Different Container</h2>
 <?php $map2->printMap() ?>
 <div id="container" style="width:500px;height:500px;"></div>
+
+<hr style="clear:both">
+<h2>Auto Open</h2>
+<?php $map3->printMap() ?>
+
 </body>
 
 </html>
