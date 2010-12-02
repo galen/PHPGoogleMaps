13b33aa1ed35331fbd3a43ccc4b5ae49f622af68
galen <galenjr@gmail.com>
2010-12-02 00:41:09 -0500

    Add getDistanceFrom() function

--- PHPGoogleMaps/Core/LatLng.php
+++ PHPGoogleMaps/Core/LatLng.php
@@ -35,4 +35,36 @@ class LatLng {
 		$this->lng = $lng;
 	}
 
+	/**
+	 * Get distance from another latlng
+	 *
+	 * @param LatLng $latlng LatLng object to get distance from.
+	 * @param float $adjustment Adjust the distance to take turns into account.
+	 *                          1.125 seems to be the most accurate.
+	 * @param string $units Units to return. Default (m) is miles.
+	 *                      n = nautical miles, k = kilometers,
+	 *                      f = feet, i = inches.
+	 * @return float Distance in the specified units
+	 */
+	public function getDistanceFrom( LatLng $latlng, $adjustment=1.125, $units='m' ) {
+		$miles = $adjustment * 3959 * acos( cos( deg2rad( $this->lat ) ) * cos( deg2rad( $latlng->lat ) ) * cos( deg2rad( $this->lng ) - deg2rad( $latlng->lng ) ) + sin( deg2rad($this->lat ) ) * sin( deg2rad( $latlng->lat ) ) );
+		switch( strtolower( $units ) ) {
+			case 'k':
+				return $miles * 1.609344;
+				break;
+			case 'n':
+				return $miles * 0.868976242;
+				break;
+			case 'f':
+				return $miles * 5280;
+				break;            
+			case 'i':
+				return $miles * 63360;
+				break;            
+			case 'm':
+			default:
+				return $miles;
+				break;
+		}
+	}
 }
