e89481714c9638f376ea94094bda8c8dd9db4582
galen <galenjr@gmail.com>
2011-04-06 19:18:47 -0400

    Add alternate marker handler example

---
+++ examples/markers_alternate_click.php
@@ -0,0 +1,91 @@
+<?php
+
+require( '../PHPGoogleMaps/Core/Autoloader.php' );
+$map_loader = new SplClassLoader('PHPGoogleMaps', '../');
+$map_loader->register();
+
+require( '_system/config.php' );
+
+$map = new \PHPGoogleMaps\Map;
+
+$locations = array(
+	'New York, NY',
+	'San Diego, CA',
+	'Dallas, TX',
+	'Seattle, WA',
+	'Miami, FL',
+	'Atlanta, GA',
+	'Boise, ID',
+	'Green Bay, WI',
+	'Detroit, MI',
+	'Denver, CO',
+	'Phoenix, AZ',
+	'Portland, OR',
+	'Chicago, IL',
+	'New Orleans, LA',
+	'San Francisco, CA',
+	'Las Vegas, NV'
+);
+
+foreach( $locations as $i => $location ) {
+	$marker = \PHPGoogleMaps\Overlay\Marker::createFromLocation( $location,
+		array(
+			'title' => $location,
+			'content' => "$location marker"
+		)
+	);
+	// You must pass the marker by reference so it can be decorated and used later to add the event listener
+	$map->addObject( &$marker );
+	
+	// You have to add the event handler after the marker has been added to a map
+	$click_handler = new \PHPGoogleMaps\Event\EventListener( $marker, 'click', 'function(){alert("You clicked " + '. $marker .'.content);}' );
+	$map->addObject( $click_handler );
+}
+
+// Diable infowindows so that only the alternate click handler gets triggered
+$map->disableInfoWindows();
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Alternate Click Handlers - <?php echo PAGE_TITLE ?></title>
+	<link rel="stylesheet" type="text/css" href="_css/style.css">
+	<?php $map->printHeaderJS() ?>
+	<?php $map->printMapJS() ?>
+</head>
+<body>
+
+<h1>Alternate Click Handlers</h1>
+<?php require( '_system/nav.php' ) ?>
+
+<p>Steps for creating alternate click handlers</p>
+<ol>
+	<li>Create a marker and add it to the map
+		<pre>
+$marker = \PHPGoogleMaps\Overlay\Marker::createFromLocation( $location,
+	array(
+		'title' => 'title',
+		'content' => 'content'
+	)
+);
+$map->addObject( &$marker );
+		</pre>
+	</li>
+	<li>Create an event listener with the alternate click handler for the marker and and add that to the map
+	<pre>
+$click_handler = new \PHPGoogleMaps\Event\EventListener( $marker, 'click', 'function(){alert("You clicked " + '. $marker .'.content);}' );
+$map->addObject( $click_handler );
+	</pre>
+	</li>
+	<li>Optionally disable infowindows
+	<pre>$map->disableInfoWindows();</pre>
+	</li>
+</ol>
+
+<?php $map->printMap() ?>
+
+</body>
+
+</html>
